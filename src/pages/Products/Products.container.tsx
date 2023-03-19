5ad8caa9700da030474c0aac98d4db0a2da7a912
Idris Dopico Peña <dopico1996@hotmail.com>
2023-03-19 12:50:47 +0100

    Create initial setup

---
+++ src/pages/Products/Products.container.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Products from "./Products";
+
+const ProductsContainer = () => {
+
+  return (
+    <Products />
+  );
+};
+
+export default ProductsContainer;
---
+++ src/pages/Products/Products.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Products = () => {
+  return <div>product page</div>;
+}
+
+export default Products;
